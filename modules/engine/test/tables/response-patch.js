9fda3b0e83895517054bff74e894cc5ffd43c774
idralyuk <idralyuk@ebaysf.com>
2011-11-05 01:54:33 -0600

    Added unit test for response patch feature.

---
+++ modules/engine/test/tables/response-patch.js
@@ -0,0 +1,34 @@
+exports['patch response'] = function(args) {
+    var body = args.body;
+    var items = [];
+
+    if (body && body.findItemsByKeywordsResponse && body.findItemsByKeywordsResponse.searchResult && body.findItemsByKeywordsResponse.searchResult.item) {
+
+        for (var i = 0; i < body.findItemsByKeywordsResponse.searchResult.item.length; i++){
+	    var result = body.findItemsByKeywordsResponse.searchResult.item[i];
+            if (result) {
+		var item = {};
+		for (var p in result) {
+		    item['_' + p + '_'] = result[p]; // rename vars for the test
+		}
+		items.push(item);
+            }
+        }
+
+	// put items into 'items' instead of 'item'
+        body.findItemsByKeywordsResponse.searchResult.items = items;
+    }
+
+    return body;
+}
+
+exports['patch status'] = function(options) {
+    var json = options.body;
+    if(json && ((json.findItemsByKeywordsResponse && json.findItemsByKeywordsResponse.ack === 'Failure') ||
+		json.errorMessage)) {
+        return 400;
+    }
+    else {
+        return 200;
+    }
+}
---
+++ modules/engine/test/tables/response-patch.ql
@@ -0,0 +1,8 @@
+-- This is used temporarily
+create table ebay.finding.items.rp
+   on select get from 'http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.8.0&GLOBAL-ID={globalid}&SECURITY-APPNAME={apikey}&RESPONSE-DATA-FORMAT={format}&REST-PAYLOAD&keywords={^keywords}&paginationInput.entriesPerPage={limit}&paginationInput.pageNumber={pageNumber}&outputSelector%280%29=SellerInfo&sortOrder={sortOrder}'
+       with aliases format = 'RESPONSE-DATA-FORMAT', json = 'JSON', xml = 'XML'
+       using defaults format = 'XML', globalid = 'EBAY-US', sortorder ='BestMatch',
+             apikey =  "{config.ebay.apikey}", limit = 10, pageNumber = 1
+       using patch 'response-patch.js'
+       resultset 'findItemsByKeywordsResponse.searchResult.items'
