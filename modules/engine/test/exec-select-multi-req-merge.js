9a2e4d0453602a24107b345f07d60bbb508eaa82
Subbu Allamaraju <subbu@ebaysf.com>
2011-11-07 08:55:12 -0800

    This change applies for tables that take one value for the primary param, but the script is feeding in multiple values for that param.
    Consider the URL for the table:
    
        http://foo.com?param={param}
    
    used with a select
    
        select * from foo where param in ('1', '2', '3')
    
    In this case, we make three requests to the URL, and merge the results field
    by field.
    
    Imagine that the response for each request looks like this
    
        {'t': 't1',
          'status': 'ok',
          'result': {
            'id': '1',
            'b': 'b1'
          }
        }
    
    By default, we merge the three responses field by field resulting in
    
        {'t': ['t1','t2','t3'],
          'status': ['ok','ok','ok'],
          'result': [{
            'id': '1',
            'b': 'b1'
          },
          {
            'id': '2',
            'b': 'b2'
          },
          {
            'id': '3',
            'b': 'b3'
          }
        }
    
    This makes sense for some APIs. Consider a different style of response.
    
        {
          'id': '1',
          'b': 'b1'
        }
    
    In this case, merging the results into an array is more useful.
    
        [{
            'id': '1',
            'b': 'b1'
          },
          {
            'id': '2',
            'b': 'b2'
          },
          {
            'id': '3',
            'b': 'b3'
          }
        ]
    
    The table can indicate which behavior it wants by prepending '#'.
    
        http://foo.com?param={#param}
    
    This change implements this variant. The example on http://ql.io also depends on
    this fix.

--- modules/engine/test/exec-select-multi-req-merge.js
+++ modules/engine/test/exec-select-multi-req-merge.js
@@ -15,18 +15,22 @@
  */
 
 var _ = require('underscore'),
-    Engine = require('lib/engine'),
+    Engine = require('../lib/engine'),
+    http = require('http'),
+    util = require('util'),
+    url = require('url'),
+    fs = require('fs'),
     sys = require('sys');
 
 var engine = new Engine({
-    tables : __dirname + '/tables',
+    tables: __dirname + '/tables',
     config: __dirname + '/config/dev.json',
     connection: 'close'
 });
 
 // This tests merging of multiple HTTP requests
 module.exports = {
-    'check-merge': function(test) {
+    'check-merge-field': function (test) {
         var script = 'create table m1\
           on select get from "http://open.api.ebay.com/shopping?callname=GetMultipleItems&responseencoding={format}&appid={^apikey}&version={^version}&IncludeSelector=Details,ItemSpecifics,ShippingCosts&ItemID={20|itemId}"\
              using defaults format = "JSON",\
@@ -48,7 +52,7 @@ module.exports = {
         "mi2_1": "{mi2_1}",\
         "mi2_2": "{mi2_2}"\
         }';
-        engine.exec(script, function(err, result) {
+        engine.exec(script, function (err, result) {
             if(err) {
                 test.fail('got error: ' + err.stack || err);
                 test.done();
@@ -59,28 +63,72 @@ module.exports = {
                 test.ok(_.isArray(result.body.mi2_1));
                 test.ok(_.isArray(result.body.mi2_2[0]));
                 // Sort by ItemID
-                var l1 = _.sortBy(result.body.mi1, function(item) {
+                var l1 = _.sortBy(result.body.mi1, function (item) {
                     return item.ItemID;
                 });
-                var l2_1 = _.sortBy(result.body.mi2_1, function(item) {
+                var l2_1 = _.sortBy(result.body.mi2_1, function (item) {
                     return item.ItemID;
                 });
-                var l2_2 = _.sortBy(result.body.mi2_2[0], function(item) {
+                var l2_2 = _.sortBy(result.body.mi2_2[0], function (item) {
                     return item.ItemID;
                 });
-                var ids1 = _.map(l1, function(item) {
+                var ids1 = _.map(l1, function (item) {
                     return item.ItemID;
                 });
-                var ids2_1 = _.map(l2_1, function(item) {
+                var ids2_1 = _.map(l2_1, function (item) {
                     return item.ItemID;
                 });
-                var ids2_2 = _.map(l2_2, function(item) {
+                var ids2_2 = _.map(l2_2, function (item) {
                     return item.ItemID;
                 });
                 test.deepEqual(ids1, ids2_1);
                 test.deepEqual(ids1, ids2_2);
                 test.done();
             }
+        });
+    },
+
+    'check-merge-block': function (test) {
+        var server = http.createServer(function (req, res) {
+            var file = __dirname + '/mock' + req.url;
+            // Remove query params
+            var parsed = url.parse(file, true);
+            file = parsed.pathname;
+            var stat = fs.statSync(file);
+            res.writeHead(200, {
+                'Content-Type': file.indexOf('.xml') >= 0 ? 'application/xml' : 'application/json',
+                'Content-Length': stat.size
+            });
+            var readStream = fs.createReadStream(file);
+            util.pump(readStream, res, function (e) {
+                if(e) {
+                    console.log(e.stack || e);
+                }
+                res.end();
+            });
+        });
+        server.listen(3000, function () {
+            // Do the test here.
+            var engine = new Engine({
+                connection: 'close'
+            });
+            var script = fs.readFileSync(__dirname + '/mock/merge-block.ql', 'UTF-8');
+            engine.exec(script, function (err, results) {
+                if(err) {
+                    console.log(err.stack || err);
+                    test.ok(false, 'Grrr');
+                    test.done();
+                }
+                else {
+                    results = results.body;
+                    test.ok(_.isArray(results));
+                    _.each(results, function(item) {
+                        test.equals(item.ProductID.Value, 99700122);
+                    });
+                    server.close();
+                    test.done();
+                }
+            });
         });
     }
 }
