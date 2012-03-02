76637392886e8c001c1d1e2152d70c3a6e076de9
Madhura Tipnis <mtipnis@ebaysf.com>
2012-03-02 13:28:47 -0800

    Fixed the select port test and added select some test

--- modules/engine/test/select-test.js
+++ modules/engine/test/select-test.js
@@ -74,9 +74,7 @@ var cooked = {
             }
         ],
         script: 'create table finditems on select get from "http://localhost:3026" '+
-            'resultset "findItemsByKeywordsResponse.searchResult.item"; '+
-            'web = select * from finditems where keywords = "ipad";'+
-            'return "{web}"',
+            'resultset "findItemsByKeywordsResponse"; ',
 
         udf: {
             test : function (test, err, result) {
@@ -84,10 +82,49 @@ var cooked = {
                     test.ok(false,'got error: ' + err.stack || err);
                 }
                 else {
+                    test.ok((result.body), { message: 'ok' });
+            }
+            }
+        }
+
+    },
+    selectsome:{
+        ports: [
+            {
+                port: 3000,
+                status: 200,
+                type: "application",
+                subType: "xml",
+                payload:
+                    '<?xml version="1.0"?>' +
+                        '<findItemsByKeywordsResponse>' +
+                        '<item><itemId>220944750971</itemId>'+
+                        '<title>Mini : Clubman S 2011 MINI COOPER S CLUBMAN*CONVENIENCE PKG,PREMIUM PKG,XENON LIGHTS=SWEET RIDE</title>'+
+                        '<primaryCategory><categoryName>Clubman</categoryName> </primaryCategory>'+
+                        '<sellingStatus> <currentPrice currencyId="USD">16000.0</currentPrice></sellingStatus></item>'+
+                        '<item><itemId>120854322916</itemId>'+
+                        '<title>COOPER CROUSE HINDS APJ6485 ARKTITE PLUG RECEPTACLE PIN SLEEVE 600V 60A 3W 43632</title>' +
+                        '<primaryCategory><categoryName>Receptacles &amp; Outlets</categoryName></primaryCategory>'+
+                        '<sellingStatus><currentPrice currencyId="USD">169.99</currentPrice></sellingStatus></item>'+
+                        '</findItemsByKeywordsResponse>'
+            }
+        ],
+        script: 'create table finditems1 on select get from "http://localhost:3000"' +
+                'resultset "findItemsByKeywordsResponse.item"; ' +
+                'web= select title, itemId, primaryCategory.categoryName,sellingStatus.currentPrice from finditems1 where keywords="cooper" and FreeShippingOnly = "true" and MinPrice = "100" ;'+
+                'return "{web}"',
+
+        udf: {
+            test : function (test, err, result) {
+                if(err) {
+                    test.fail('got error: ' + err.stack || err);
+                }
+                else {
                     test.equals(result.headers['content-type'], 'application/json', 'HTML expected');
                     test.ok(_.isArray(result.body), 'expected an array');
                     test.ok(result.body.length > 0, 'expected some items');
-                    test.ok(!_.isArray(result.body[0]), 'expected object in the array');
+                    test.ok(_.isArray(result.body[0]), 'expected array in the array');
+                    test.equals(4, result.body[0].length, 'expected four fields');
                 }
             }
         }
