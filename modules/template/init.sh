2d19d47b118ccf7914e2246c34d6901ca27b55eb
Michael Stillwell <mjs@beebo.org>
2012-03-02 00:37:30 +0000

    Fix URL--previous URL gives a 404

--- modules/template/init.sh
+++ modules/template/init.sh
@@ -6,7 +6,7 @@
 #
 # You can run this in a new directory by
 #
-# curl "https://github.com/ql-io/ql.io/blob/master/template/init.sh" | sh
+# curl "https://raw.github.com/ql-io/ql.io/master/modules/template/init.sh" | sh
 #
 git clone git@github.com:ql-io/ql.io-template.git ql.io-template
 mv ql.io-template/* .
