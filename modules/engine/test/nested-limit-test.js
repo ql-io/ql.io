7b0073756b57fef1890dbc73741ec1cfae31d373
Madhura Tipnis <mtipnis@ebaysf.com>
2012-03-22 12:51:17 -0700

    Renaming and adding the required config file

--- modules/engine/test/nested-limit-test.js
+++ modules/engine/test/nested-limit-test.js
@@ -14,136 +14,65 @@
  * limitations under the License.
  */
 
-"use strict";
+var _    = require('underscore'),
+    util = require('util'),
+    http = require('http');
 
-var _ = require('underscore'),
-    Engine = require('../lib/engine');
+var Engine = require('../lib/engine');
 
-var engine = new Engine({
-    tables : __dirname + '/tables',
-    config: __dirname + '/config/dev.json'
-});
+var maxNestedRequests =  2 ;
 
-var maxNestedRequests = engine.config.maxNestedRequests || 50, limit = 10;
+var cooked = {
+    maxinclausetest : {
+        ports: [
+            {
+                port: 3000,
+                status: 200,
+                type: "application",
+                subType: "json",
+                payload:
+                    JSON.stringify(
+                        [   {"id" : "1",
+                                "name" : "A"},
+                            {"id" : "2",
+                                "name" : "B"},
+                            {"id" : "3",
+                                "name" : "C"},
+                            {"id" : "4",
+                                "name" : "D"},
+                            {"id" : "5",
+                                "name" : "E"}]
+                    )
+            }
+        ],
+        script: 'create table payload1 on select get from "http://localhost:3000/foo?id={id}"'+
+                'return select * from payload1 where id in (1, 2, 3, 4, 5)',
+        udf: {
 
-module.exports = {
-    'max-in-clause-test' : function(test) {
-        var q = 'select * from ebay.finding.items.many.results where keywords in (' + keywords.toString() + ')';
-        engine.exec(q, function(err, list) {
-            if(err) {
-                test.fail('got error ' + err);
-                test.done();
+            test : function (test, err, result) {
+                if(err) {
+                    test.fail('got error: ' + err.stack || err);
+
+                }
+                else {
+                    test.ok(_.isObject(result.body), 'expected an object');
+                    test.equals(result.headers['content-type'], 'application/json', 'HTML expected');
+                    test.ok(_.isArray(result.body), 'expected an array');
+                    test.ok(result.body.length > 0, 'expected some items');
+                    test.ok(!_.isArray(result.body[0]), 'expected object in the array');
+                    test.equals(result.body.length, maxNestedRequests * 5, 'expected a different number of results for in-clause');
+
+                }
             }
-            else {
-                test.equals(list.headers['content-type'], 'application/json', 'HTML expected');
-                test.ok(_.isArray(list.body), 'expected an array');
-                test.ok(list.body.length > 0, 'expected some items');
-                test.ok(!_.isArray(list.body[0]), 'expected object in the array');
-                test.equals(list.body.length, maxNestedRequests * limit, 'expected a different number of results for in-clause');
-                test.done();
-            }
-        });
-    },
-    'max-funcs-test' : function(test) {
-        var q = 'select a.ItemID as itemId, b.keywords as keywords from ebay.finding.items as a, ebay.finding.items.many.results as b where a.keywords = b.keywords and a.keywords  in (' + keywords.toString() + ')';
-        engine.exec(q, function(err, list) {
-            if(err) {
-                test.fail('got error ' + err);
-                test.done();
-            }
-            else {
-                test.equals(list.headers['content-type'], 'application/json', 'HTML expected');
-                test.ok(_.isArray(list.body), 'expected an array');
-                test.ok(list.body.length > 0, 'expected some items');
-                test.ok(!_.isArray(list.body[0]), 'expected object in the array');
-                test.equals(list.body.length, maxNestedRequests, 'expected a different number of results for funcs');
-                test.done();
-            }
-        });
+        }
+
     }
+
 }
 
-// Define 80 keywords. By default only 50 should be queried.
-var keywords = [
-    'roman',
-    'ring',
-    'egyptian',
-    'greek',
-    'chinese',
-    'bronze',
-    'gold',
-    'intaglio',
-    'antique',
-    'glass',
-    'jade',
-    'japanese',
-    'buddha',
-    'bronze',
-    'ivory',
-    'chinese',
-    'painting',
-    'netsuke',
-    'sword',
-    'vase',
-    'table',
-    'chairs',
-    'oak',
-    'desk',
-    'bed',
-    'chair',
-    'french',
-    'sofa',
-    'armoire',
-    'dresser',
-    'doll',
-    'primitive',
-    'wood',
-    'sign',
-    'antique',
-    'halloween',
-    'basket',
-    'table',
-    'box',
-    'americana',
-    'tea',
-    'tiffany',
-    'sterling',
-    'rogers',
-    'silver',
-    'coin',
-    'tray',
-    'flatware',
-    'gorham',
-    'coin',
-    'door',
-    'stained',
-    'cast',
-    'iron',
-    'window',
-    'tile',
-    'victorian',
-    'shabby',
-    'light',
-    'garden',
-    'bronze',
-    'french',
-    'deco',
-    'lamp',
-    'crafts',
-    'vase',
-    'mirror',
-    'frame',
-    'antique',
-    'nouveau',
-    'atlas',
-    'globe',
-    'texas',
-    'italy',
-    'world',
-    'france',
-    'germany',
-    'maps',
-    'poland',
-    'map'
-]
-
+module.exports = require('ql-unit').init({
+    cooked: cooked,
+    engine:new Engine({
+        config: __dirname + '/mock/nested-limit-test.json'
+    })
+});
