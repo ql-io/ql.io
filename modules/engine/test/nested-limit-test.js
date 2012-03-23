d476b4094c5e59681b1f83d73d259ee66b3679b2
Madhura Tipnis <mtipnis@ebaysf.com>
2012-03-23 10:14:00 -0700

    Made necessary changes

--- modules/engine/test/nested-limit-test.js
+++ modules/engine/test/nested-limit-test.js
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+"use strict";
 
 var _    = require('underscore'),
     util = require('util'),
