a184b430959d6e25249624e73d7d79583ce1b919
Subbu Allamaraju <subbu@ebaysf.com>
2012-05-22 21:31:15 -0700

    Add fallback for the language

--- modules/compiler/lib/peg/ql.js
+++ modules/compiler/lib/peg/ql.js
@@ -41,6 +41,7 @@ module.exports = (function(){
         "LineCrlf": parse_LineCrlf,
         "Line": parse_Line,
         "Assign": parse_Assign,
+        "Fallback": parse_Fallback,
         "StatementOrObject": parse_StatementOrObject,
         "CallUdf": parse_CallUdf,
         "Statement": parse_Statement,
@@ -132,6 +133,7 @@ module.exports = (function(){
         "Ret": parse_Ret,
         "Return": parse_Return,
         "RetVal": parse_RetVal,
+        "RetFallback": parse_RetFallback,
         "RetRef": parse_RetRef,
         "Route": parse_Route,
         "OptParam": parse_OptParam,
@@ -454,7 +456,7 @@ module.exports = (function(){
         result0 = parse_Output();
         result0 = result0 !== null ? result0 : "";
         if (result0 !== null) {
-          result1 = parse_StatementOrObject();
+          result1 = parse_Fallback();
           if (result1 !== null) {
             result2 = parse_insig();
             if (result2 !== null) {
@@ -503,6 +505,80 @@ module.exports = (function(){
             symbols[a.assign] = {};
             return s;
         })(pos0.offset, pos0.line, pos0.column, result0[0], result0[1]);
+        }
+        if (result0 === null) {
+          pos = clone(pos0);
+        }
+        return result0;
+      }
+
+      function parse_Fallback() {
+        var result0, result1, result2, result3, result4;
+        var pos0, pos1, pos2, pos3;
+
+        pos0 = clone(pos);
+        pos1 = clone(pos);
+        result0 = parse_StatementOrObject();
+        if (result0 !== null) {
+          result1 = parse_insig();
+          if (result1 !== null) {
+            pos2 = clone(pos);
+            pos3 = clone(pos);
+            if (input.substr(pos.offset, 2) === "||") {
+              result2 = "||";
+              advance(pos, 2);
+            } else {
+              result2 = null;
+              if (reportFailures === 0) {
+                matchFailed("\"||\"");
+              }
+            }
+            if (result2 !== null) {
+              result3 = parse_insig();
+              if (result3 !== null) {
+                result4 = parse_Fallback();
+                if (result4 !== null) {
+                  result2 = [result2, result3, result4];
+                } else {
+                  result2 = null;
+                  pos = clone(pos3);
+                }
+              } else {
+                result2 = null;
+                pos = clone(pos3);
+              }
+            } else {
+              result2 = null;
+              pos = clone(pos3);
+            }
+            if (result2 !== null) {
+              result2 = (function(offset, line, column, f) { return f; })(pos2.offset, pos2.line, pos2.column, result2[2]);
+            }
+            if (result2 === null) {
+              pos = clone(pos2);
+            }
+            result2 = result2 !== null ? result2 : "";
+            if (result2 !== null) {
+              result0 = [result0, result1, result2];
+            } else {
+              result0 = null;
+              pos = clone(pos1);
+            }
+          } else {
+            result0 = null;
+            pos = clone(pos1);
+          }
+        } else {
+          result0 = null;
+          pos = clone(pos1);
+        }
+        if (result0 !== null) {
+          result0 = (function(offset, line, column, s1, s2) {
+            if(s2) {
+                s1.fallback = s2;
+            }
+            return s1;
+        })(pos0.offset, pos0.line, pos0.column, result0[0], result0[2]);
         }
         if (result0 === null) {
           pos = clone(pos0);
@@ -5272,7 +5348,7 @@ module.exports = (function(){
         if (result0 !== null) {
           result1 = parse_insig();
           if (result1 !== null) {
-            result2 = parse_RetVal();
+            result2 = parse_RetFallback();
             if (result2 !== null) {
               result3 = parse_insig();
               if (result3 !== null) {
@@ -5353,6 +5429,80 @@ module.exports = (function(){
               result0 = parse_Value();
             }
           }
+        }
+        return result0;
+      }
+
+      function parse_RetFallback() {
+        var result0, result1, result2, result3, result4;
+        var pos0, pos1, pos2, pos3;
+
+        pos0 = clone(pos);
+        pos1 = clone(pos);
+        result0 = parse_RetVal();
+        if (result0 !== null) {
+          result1 = parse_insig();
+          if (result1 !== null) {
+            pos2 = clone(pos);
+            pos3 = clone(pos);
+            if (input.substr(pos.offset, 2) === "||") {
+              result2 = "||";
+              advance(pos, 2);
+            } else {
+              result2 = null;
+              if (reportFailures === 0) {
+                matchFailed("\"||\"");
+              }
+            }
+            if (result2 !== null) {
+              result3 = parse_insig();
+              if (result3 !== null) {
+                result4 = parse_RetFallback();
+                if (result4 !== null) {
+                  result2 = [result2, result3, result4];
+                } else {
+                  result2 = null;
+                  pos = clone(pos3);
+                }
+              } else {
+                result2 = null;
+                pos = clone(pos3);
+              }
+            } else {
+              result2 = null;
+              pos = clone(pos3);
+            }
+            if (result2 !== null) {
+              result2 = (function(offset, line, column, f) { return f; })(pos2.offset, pos2.line, pos2.column, result2[2]);
+            }
+            if (result2 === null) {
+              pos = clone(pos2);
+            }
+            result2 = result2 !== null ? result2 : "";
+            if (result2 !== null) {
+              result0 = [result0, result1, result2];
+            } else {
+              result0 = null;
+              pos = clone(pos1);
+            }
+          } else {
+            result0 = null;
+            pos = clone(pos1);
+          }
+        } else {
+          result0 = null;
+          pos = clone(pos1);
+        }
+        if (result0 !== null) {
+          result0 = (function(offset, line, column, s1, s2) {
+            if(s2) {
+                s1.fallback = s2;
+            }
+            return s1;
+        })(pos0.offset, pos0.line, pos0.column, result0[0], result0[2]);
+        }
+        if (result0 === null) {
+          pos = clone(pos0);
         }
         return result0;
       }
